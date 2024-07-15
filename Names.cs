0864fe333397c60fb0ffc9e477d5ee43cc833829
Amin Ghasemi <mmnghasemi@gmail.com>
2024-07-14 16:32:48 -0800

    Add project files.

---
+++ Names.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace EnumrableClass
+{
+    public class Names : IEnumerable
+    {
+        string[] names = { "Amin", "Rebecca", "Ghorme Sabzi", "Pishol" };
+
+        public IEnumerator GetEnumerator()
+        {
+            Enumerator enumerator = new Enumerator(names);
+            return enumerator;
+        }
+    }
+
+    public class Enumerator : IEnumerator
+    {
+        string[] collection;
+
+        public Enumerator(string[] collection)
+        {
+            this.collection = collection;
+        }
+
+        int index = -1;
+        public object Current => collection[index];
+
+        public bool MoveNext()
+        {
+            if (index < collection.Length - 1)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+// Creat a class with IEnumerable interface
+
+using EnumrableClass;
+
+Console.WriteLine("Hello World!");
+Names obj = new Names();
+
+foreach (var item in obj)
+{
+    Console.WriteLine(item);
+}
+Console.ReadKey();
+
